3e48c89bad51c930cf43b98c5e65a412815077d7
Fravic <fravicf@gmail.com>
2013-04-01 22:44:47 -0400

    start of time diff parsing

--- project/histograms/parse.rb
+++ project/histograms/parse.rb
@@ -1,8 +1,11 @@
+require 'date'
+
 class Parser
   def initialize(infile)
     lines = File.readlines(infile)
     lines.map!{ |l| l.encode!('UTF-8', 'UTF-8', :invalid => :replace) }
     @sizes = parseSizes(lines)
+    @times = parseTimes(lines)
   end
 
   def parseSizes(lines)
@@ -12,11 +15,28 @@ class Parser
     }
   end
 
-  def write(outfile)
-    File.open(outfile, 'w') { |f|
+  def parseTimes(lines)
+    re = /\d{2}:\d{2}:\d{2}.\d{6}/
+    lastTime = DateTime.parse(re.match(lines[0])[0])
+    lines.map do |l|
+      time = re.match(l)
+      next if time.nil?
+      time = DateTime.parse(time[0])
+      lastTime = time
+      time - lastTime
+    end
+  end
+
+  def write(sizesFile, timesFile)
+    File.open(sizesFile, 'w') { |f|
+      f.puts("Size,")
       @sizes.each { |l| f.puts(l + ',') }
+    }
+    File.open(timesFile, 'w') { |f|
+      f.puts("TimeDiff,")
+      @times.each { |l| f.puts(l.to_s + ',') }
     }
   end
 end
 
-Parser.new(ARGV[0]).write(ARGV[1])
+Parser.new(ARGV[0]).write(ARGV[1], ARGV[2])
