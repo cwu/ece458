2c5da6a8530abbb9a133bebaa3526a98d5a78a47
Fravic <fravicf@gmail.com>
2013-04-04 03:41:17 -0400

    updating charts

--- project/histograms/index.js
+++ project/histograms/index.js
@@ -12,24 +12,36 @@ $(function($) {
             return decodeURIComponent(results[1].replace(/\+/g, " "));
     }
 
-    var sizes = polyjs.data.url('./dumps/' +
-                                getParameterByName('src') +
-                                '.csv');
+    function createChart() {
+        return polyjs.chart(getChartSpec());
+    };
     
-    polyjs.chart({
-        layer: {
-            data: sizes,
-            type: "bar",
-            x: "bin(Size, 15)",
-            y: "count(Size)",
-        },
-        title: "Frequency of Packet Sizes",
-        dom: "chart",
-        width: 1300,
-        height: 600,
-        guide: {
-            x: { title: 'Packet Size' },
-            y: { title: 'Frequency' }
-        }
-    });
+    function updateChart(chart) {
+        chart.make(getChartSpec());
+    }
+
+    function getChartSpec() {
+        var sizes = polyjs.data.url('./dumps/' +
+                                    getParameterByName('src') +
+                                    '.csv');
+        return {
+            layer: {
+                data: sizes,
+                type: "bar",
+                x: "bin(Size, 15)",
+                y: "count(Size)",
+            },
+            title: "Frequency of Packet Sizes",
+            dom: "chart",
+            width: 1300,
+            height: 600,
+            guide: {
+                x: { title: 'Packet Size' },
+                y: { title: 'Frequency' }
+            }
+        };
+    }
+
+    var chart = createChart();
+    setInterval(function() { updateChart(chart); }, 1000);
 });
