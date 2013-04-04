0ea73ed7477791b585088d338f00cc69afb3f6be
Chris Wu <me@chriswu.me>
2013-04-04 12:38:34 -0400

    take from file

--- project/src/simulation.py
+++ project/src/simulation.py
@@ -3,6 +3,7 @@ import random
 import time
 import socket
 import morph_socket
+import sys
 
 def get_rand_size(distr):
   r = random.random()
@@ -16,15 +17,26 @@ def get_rand_size(distr):
   raise ValueError("bad random with distribution: %f" % r)
 
 def main():
-  sim_distr = [float(prob) for prob in morpheus.get_distr_from_file("data/https_cs_distr.txt")]
+  if len(sys.argv) < 2:
+    print "simulation.py <distr path>"
+    return
+
+  distr_name = sys.argv[1]
+  sim_distr = [float(prob) for prob in morpheus.get_distr_from_file(distr_name)]
 
   sock = morph_socket.MorphSocket(socket.AF_INET, socket.SOCK_STREAM)
   sock.connect(('localhost', 8081))
+  last_load = time.time()
+
   while True:
     size = get_rand_size(sim_distr)
     print 'from %d' % size
     sock.send("x" * size)
-    time.sleep(0.1)
+    time.sleep(0.01)
+
+    if time.time() - last_load >= 1:
+      sim_distr = [float(prob) for prob in morpheus.get_distr_from_file(distr_name)]
+      last_load = time.time()
 
 if __name__ == '__main__':
   main()
